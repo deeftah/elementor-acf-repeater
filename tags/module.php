e30b0e925e5af477d5a1cbb050e6ba0f8b962949
Justin Kucerak <jmkucerak@gmail.com>
2019-05-09 19:03:48 -0400

    Method stubs

--- tags/module.php
+++ tags/module.php
@@ -5,5 +5,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Module {
+	public function __construct() {
+	}
 
+	public function enqueue_scripts() {
+	}
+
+	public function register_ajax_actions() {
+	}
+
+	public function ajax_update( $params ) {
+	}
+
+	public static function get_tag_classes_names() {
+	}
+
+	public static function get_control_options( $types ) {
+	}
 }
