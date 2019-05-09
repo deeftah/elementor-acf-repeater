c480898e3d9ae0cb052c05c009160ba709a7df0d
Justin Kucerak <jmkucerak@gmail.com>
2019-05-09 19:16:48 -0400

    ajax_update method.

--- tags/module.php
+++ tags/module.php
@@ -45,7 +45,43 @@ class Module {
 		$ajax->register_ajax_action( 'update_dynamic_tag_controls', [ $this, 'ajax_update' ] );
 	}
 
+	/**
+	 * Handles ajax request to update Dynamic Tags.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @param array $params Data used for the ajax request.
+	 * @return array $result Array of options.
+	 */
 	public function ajax_update( $params ) {
+		// Bail early if a post_id was not given.
+		if ( ! isset( $params['post_id'] ) ) {
+			return [ 'error' => 'No post id given.' ];
+		}
+
+		// Instantiate an empty result.
+		$result = [
+			'tags' => [],
+		];
+		// Store all the registered tag classes.
+		$tag_names = self::get_tag_classes_names();
+
+		// Iterate over the tag classes.
+		foreach ( $tag_names as $name ) {
+			// Convert class name to file name.
+			$class_file = strtolower( str_replace( '_', '-', $name ) );
+			include_once $class_file . '.php'; // Include tag class file.
+
+			// Store supported types for the tag.
+			$types = $name::get_fields();
+
+			// Store the control options for the tag.
+			$result['tags'][ $class_file ] = self::get_control_options( $types );
+		}
+
+		return $result;
 	}
 
 	public static function get_tag_classes_names() {
