e2bfac0c63b4d4f440cee038b0b5bf74e9be7c11
Justin Kucerak <jmkucerak@gmail.com>
2019-05-09 16:48:31 -0400

    Main plugin file stub.

---
+++ elementor-acf-repeater.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Elementor ACF Repeater
+ * Description: Allows ACF repeater field values to be used in Elementor via Dynamic Tags.
+ * Version:     1.0.0
+ * Author:      Justin Kucerak
+ * Text Domain: elementor-acf-repeater
+ * 
+ */
+
+class Elementor_ACF_Repeater {
+    
+}
