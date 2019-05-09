b4a1f6903ce4602067ce6e6b6ce9e1d7b5b18466
Justin Kucerak <jmkucerak@gmail.com>
2019-05-09 17:11:21 -0400

    Main plugin file member const and variables.

--- elementor-acf-repeater.php
+++ elementor-acf-repeater.php
@@ -11,6 +11,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Responsible for 
+ */
 class Elementor_ACF_Repeater {
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
 
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '5.6';
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var Elementor_ACF_Repeater The single instance of the class.
+	 */
+	private static $_instance = null;
 }
