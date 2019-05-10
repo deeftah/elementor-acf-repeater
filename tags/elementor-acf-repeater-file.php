c157a2c2155ee9566ad7302c5c3432e3b493f291
Justin Kucerak <jmkucerak@gmail.com>
2019-05-10 00:48:58 -0400

    ACF Repeater File definition.

--- tags/elementor-acf-repeater-file.php
+++ tags/elementor-acf-repeater-file.php
@@ -4,6 +4,81 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-class Elementor_ACF_Repeater_File extends ElementorPro\Modules\DynamicTags\ACF\Tags\ACF_File {
+require_once 'elementor-acf-repeater-image.php';
 
+/**
+ * Defines the ACF Repeater File dynamic tag.
+ */
+class Elementor_ACF_Repeater_File extends Elementor_ACF_Repeater_Image {
+	/**
+	 * ACF fields supported by this tag.
+	 *
+	 * @var array $supported_fields
+	 */
+	public static $supported_fields = [
+		'file',
+	];
+
+	/**
+	 * Get Name
+	 *
+	 * Returns the Name of the tag
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string
+	 */
+	public function get_name() {
+		return 'elementor-acf-repeater-file';
+	}
+
+	/**
+	 * Get Title
+	 *
+	 * Returns the title of the Tag
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return __( 'ACF Repeater File', 'elementor-pro' );
+	}
+
+	/**
+	 * Register Controls
+	 *
+	 * Registers the Dynamic tag controls
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return void
+	 */
+	protected function _register_controls() {
+		$this->add_control(
+			'repeater_field',
+			[
+				'label'      => __( 'Repeater Field', 'elementor-pro' ),
+				'type'       => \Elementor\Controls_Manager::SELECT,
+				'groups'     => Module::get_control_options( self::$supported_fields ),
+			]
+		);
+	}
+
+	/**
+	 * Render
+	 *
+	 * Returns the supported fields for the tag.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array
+	 */
+	protected function get_supported_fields() {
+		return self::$supported_fields;
+	}
 }
